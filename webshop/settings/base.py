431a0abc5f11cfc336277401ae03bdb885b596ed
Calen Wu <calenwuc@gmail.com>
2021-07-04 23:34:09 +0200

    fix: set default language to german

--- webshop/settings/base.py
+++ webshop/settings/base.py
@@ -203,7 +203,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en'
+LANGUAGE_CODE = 'de'
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
